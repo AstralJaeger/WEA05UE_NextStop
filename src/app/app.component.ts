b7807d215b0ace830d94dbd5ad77778b7eb71a4c
Felix Hillebrand <astraljaeger@pm.me>
2025-01-07 16:49:38 +0100

    Add Tailwind CSS configuration and integrate Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbar, MatIcon],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'untitled';
+  title = 'NextStop';
 }
